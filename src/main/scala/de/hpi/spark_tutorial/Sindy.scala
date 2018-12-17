db98c75f0533cea2a8526290c8ba6ab6c501cf02
Alexander Riese <alexander.riese@student.hpi.de>
2018-12-17 11:24:25 +0100

    solve homework

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -6,6 +6,29 @@ object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
 
-    // TODO
+    import spark.implicits._
+
+    val datasets = inputs.map(file =>
+      spark.read
+        .option("inferSchema", "false")
+        .option("header", "true")
+        .option("sep", ";")
+        .csv(file)
+    )
+
+    val cells = datasets.map(dataSet => {
+      val datasetColumns = dataSet.columns
+      dataSet.flatMap(row => row.toSeq.asInstanceOf[Seq[String]].zip(datasetColumns))
+    }).reduce(_ union _).distinct()
+
+    val attributeSets = cells.rdd.groupByKey().mapValues(_.toSet).values.distinct()
+
+    val inclusionLists = attributeSets.flatMap(attributeSet =>
+      attributeSet.map(x => (x , attributeSet - x))
+    )
+
+    val results = inclusionLists.reduceByKey(_ intersect _).filter(_._2.nonEmpty)
+
+    results.foreach(resultPair => println(s"${resultPair._1} < ${resultPair._2.mkString(", ")}"))
   }
 }
